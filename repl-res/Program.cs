ab31ee8dbb3f8d9da57158786456078926f146c4
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2022-05-05 19:56:41 -0400

    [repl-res] Quick tool to add/replace a resource inside an assembly

---
+++ repl-res/Program.cs
@@ -0,0 +1,15 @@
+﻿using Mono.Cecil;
+
+AssemblyDefinition ad = AssemblyDefinition.ReadAssembly (args [0]);
+EmbeddedResource er = new (args [1], ManifestResourceAttributes.Public, File.ReadAllBytes (args [1]));
+var mm = ad.MainModule;
+if (mm.HasResources) {
+	for (int i = 0; i < mm.Resources.Count; i++) {
+		if (mm.Resources [i].Name == er.Name) {
+			mm.Resources.RemoveAt (i);
+			break;
+		}
+	}
+}
+mm.Resources.Add (er);
+ad.Write (args [0]);
---
+++ repl-res/README.md
@@ -0,0 +1,7 @@
+Add or replace a public resource inside an assembly
+
+```
+$ dotnet run -- assembly.dll resource.file
+```
+
+Note: will overwrite the original file
